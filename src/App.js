c17a4c2cec426c29d793f3632ecbeda5386b52c8
fuadmoin <fuadamoin@gmail.com>
2023-07-18 14:19:12 +0300

    Init.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="content font-bold">This is the content.</p>
+    </div>
+  );
+}
+
+export default App;
